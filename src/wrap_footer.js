5ce6f2de9bac9515f5cf3f84373fe8282d2770d5
James Coleman <james.coleman@bugherd.com>
2016-12-19 11:33:54 +1100

    remove require function; fixes elevio/warne#13

--- src/wrap_footer.js
+++ src/wrap_footer.js
@@ -1,20 +1,8 @@
 /* jshint ignore:start */
 if (callback) {
-    if(typeof define === 'function' && define.amd){
-        //For backwards compatability
-        var n_callback = callback;
-        if (typeof requirejs === 'function') {
-            requirejs(["strophe"], function(o){
-                n_callback(o.Strophe,o.$build,o.$msg,o.$iq,o.$pres);
-            });
-        } else {
-            require(["strophe"], function(o){
-                n_callback(o.Strophe,o.$build,o.$msg,o.$iq,o.$pres);
-            });
-        }
-    }else{
-        return callback(Strophe, $build, $msg, $iq, $pres);
-    }
+    require(["strophe"], function(o){
+        callback(o.Strophe,o.$build,o.$msg,o.$iq,o.$pres);
+    });
 }
 
 
